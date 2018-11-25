00dfa8553169235d72e6c53efb776ed8bad59d29
jayesslin <493965578@qq.com>
2018-11-24 18:46:49 -0600

    Update 树_判断是否是平衡二叉树.java

--- src/剑指offer66/树_判断是否是平衡二叉树.java
+++ src/剑指offer66/树_判断是否是平衡二叉树.java
@@ -1,5 +1,27 @@
 package ½£Ö¸offer66;
 
-public class Ê÷_ÅÐ¶ÏÊÇ·ñÊÇÆ½ºâ¶þ²æÊ÷ {
+import model.TreeNode;
 
+public class Ê÷_ÅÐ¶ÏÊÇ·ñÊÇÆ½ºâ¶þ²æÊ÷ {
+	public class TreeNode {
+	    int val = 0;
+	    TreeNode left = null;
+	    TreeNode right = null;
+
+	    public TreeNode(int val) {
+	        this.val = val;
+	    }
+	public boolean IsBalanced_Solution(TreeNode root) {
+		if(root==null) return true;
+		return max(root)-min(root)>1 ? false: true;
+    }
+	public int max(TreeNode root) {
+		if(root==null) return 0;
+		return Math.max(1+max(root.left), 1+max(root.right));
+	}
+	public int min(TreeNode root) {
+		if(root==null) return 0;
+		return Math.min(1+max(root.left), 1+max(root.right));
+	}
+}
 }
